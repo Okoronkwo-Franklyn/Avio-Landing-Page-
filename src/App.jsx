ff4ea1fc4863154fa2075012a5bb1cd15ecb272f
DevFrank <franklynokoronkwo104@gmail.com>
2026-08-15 13:08:05 +0100

    feat: Added Insights, MembershipGrid and Pricing components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Hero from "./components/Hero"
 import HeroCards from "./components/HeroCards"
 import FeatureList from "./components/FeatureList"
 import Programs from "./components/Programs"
+import Insights from "./components/Insights"
+import Pricing from "./components/Pricing"
+import MembershipGrid from "./components/MembershipGrid"
 
 function App() {
   return(
@@ -13,6 +16,9 @@ function App() {
       <HeroCards />
       <FeatureList />
       <Programs />
+      <Insights />
+      <Pricing />
+      <MembershipGrid />
 
     </>
   )
