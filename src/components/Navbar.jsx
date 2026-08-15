a3262d99824bb624ddaffcc858708d996e12a279
DevFrank <franklynokoronkwo104@gmail.com>
2026-08-15 04:42:31 +0100

    feat: Added Navbar shell for my Precious to use

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+    return(
+        <header className="sticky top-0 z-50 w-full bg-white">
+
+            <nav className="mx-auto flex max-w-7xl items-center justify-between px-6 py-5 lg:px-8">
+
+                {/*LOGO, NAVLINKS, ARROWS, LOGIN/BUTTONS*/} 
+                <h2>NAVBAR GOES HERE</h2> 
+
+            </nav>
+
+        </header>
+    )
+}
+
+export default Navbar
