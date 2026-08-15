e2a4e5149de0a182e57233c17e72f1480a308353
Uche Emmanuel <ucheemmanuel20222@gmail.com>
2026-08-15 16:35:45 +0100

    Changed activity score card
    :wq#	src/components/HeroCards.jsx
    
    :wq#

--- src/components/HeroCards.jsx
+++ src/components/HeroCards.jsx
@@ -40,7 +40,7 @@ function HeroCards() {
 
           {/* Saving */}
           <div className="text-right">
-            <p className=" text-white/40 ">Saved $600</p>
+            <p className="text-[7px] text-white/40 sm:text-[8px]">Saved $580</p>
 
             <p className="mt-1 text-white/70 ]">
               in benefits
