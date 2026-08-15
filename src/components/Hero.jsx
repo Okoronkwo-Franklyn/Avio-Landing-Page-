120e4d5dc2d40a38591df088367a645e72f02580
DevFrank <franklynokoronkwo104@gmail.com>
2026-08-15 04:54:30 +0100

    feat: added Hero template for co-devs

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+function Hero() {
+  return (
+    <section className="mx-auto max-w-7xl px-6 pb-16 pt-8 lg:px-8">
+
+      <div className="grid grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-16">
+
+        <h1>HERO SECTION</h1>
+
+      </div>
+    </section>
+  );
+}
+
+export default Hero
